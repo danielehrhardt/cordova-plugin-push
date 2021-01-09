12c16dd97b8c27a01c4fd970550dc293c39fa2dc
Daniel Ehrhardt <daniel.ehrhardt@codext.de>
2021-01-09 22:17:56 +0100

    Fixed Android X Issues

--- src/android/com/adobe/phonegap/push/PushPlugin.java
+++ src/android/com/adobe/phonegap/push/PushPlugin.java
@@ -12,12 +12,14 @@ import android.media.AudioAttributes;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.onesignal.shortcutbadger.ShortcutBadger;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
@@ -35,7 +37,6 @@ import java.util.Iterator;
 import java.util.ArrayList;
 import java.util.List;
 
-import me.leolin.shortcutbadger.ShortcutBadger;
 
 public class PushPlugin extends CordovaPlugin implements PushConstants {
 
